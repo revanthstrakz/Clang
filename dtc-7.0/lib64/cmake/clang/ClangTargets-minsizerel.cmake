7091fecc013aafa7b3cbe825d3167b3f9bf8a2c2
Martichou <martichou.andre@gmail.com>
2018-05-03 19:05:06 +0200

    Update

---
+++ dtc-7.0/lib64/cmake/clang/ClangTargets-minsizerel.cmake
@@ -0,0 +1,344 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "MinSizeRel".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "clangBasic" for configuration "MinSizeRel"
+set_property(TARGET clangBasic APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangBasic PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangBasic.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangBasic )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangBasic "${_IMPORT_PREFIX}/lib64/libclangBasic.a" )
+
+# Import target "clangLex" for configuration "MinSizeRel"
+set_property(TARGET clangLex APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangLex PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangLex.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangLex )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangLex "${_IMPORT_PREFIX}/lib64/libclangLex.a" )
+
+# Import target "clangParse" for configuration "MinSizeRel"
+set_property(TARGET clangParse APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangParse PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangParse.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangParse )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangParse "${_IMPORT_PREFIX}/lib64/libclangParse.a" )
+
+# Import target "clangAST" for configuration "MinSizeRel"
+set_property(TARGET clangAST APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangAST PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangAST.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangAST )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangAST "${_IMPORT_PREFIX}/lib64/libclangAST.a" )
+
+# Import target "clangDynamicASTMatchers" for configuration "MinSizeRel"
+set_property(TARGET clangDynamicASTMatchers APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangDynamicASTMatchers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangDynamicASTMatchers.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangDynamicASTMatchers )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangDynamicASTMatchers "${_IMPORT_PREFIX}/lib64/libclangDynamicASTMatchers.a" )
+
+# Import target "clangASTMatchers" for configuration "MinSizeRel"
+set_property(TARGET clangASTMatchers APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangASTMatchers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangASTMatchers.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangASTMatchers )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangASTMatchers "${_IMPORT_PREFIX}/lib64/libclangASTMatchers.a" )
+
+# Import target "clangCrossTU" for configuration "MinSizeRel"
+set_property(TARGET clangCrossTU APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangCrossTU PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangCrossTU.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangCrossTU )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangCrossTU "${_IMPORT_PREFIX}/lib64/libclangCrossTU.a" )
+
+# Import target "clangSema" for configuration "MinSizeRel"
+set_property(TARGET clangSema APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangSema PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangSema.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangSema )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangSema "${_IMPORT_PREFIX}/lib64/libclangSema.a" )
+
+# Import target "clangCodeGen" for configuration "MinSizeRel"
+set_property(TARGET clangCodeGen APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangCodeGen PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangCodeGen.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangCodeGen )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangCodeGen "${_IMPORT_PREFIX}/lib64/libclangCodeGen.a" )
+
+# Import target "clangAnalysis" for configuration "MinSizeRel"
+set_property(TARGET clangAnalysis APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangAnalysis PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangAnalysis.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangAnalysis )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangAnalysis "${_IMPORT_PREFIX}/lib64/libclangAnalysis.a" )
+
+# Import target "clangEdit" for configuration "MinSizeRel"
+set_property(TARGET clangEdit APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangEdit PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangEdit.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangEdit )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangEdit "${_IMPORT_PREFIX}/lib64/libclangEdit.a" )
+
+# Import target "clangRewrite" for configuration "MinSizeRel"
+set_property(TARGET clangRewrite APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangRewrite PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangRewrite.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangRewrite )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangRewrite "${_IMPORT_PREFIX}/lib64/libclangRewrite.a" )
+
+# Import target "clangARCMigrate" for configuration "MinSizeRel"
+set_property(TARGET clangARCMigrate APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangARCMigrate PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangARCMigrate.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangARCMigrate )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangARCMigrate "${_IMPORT_PREFIX}/lib64/libclangARCMigrate.a" )
+
+# Import target "clangDriver" for configuration "MinSizeRel"
+set_property(TARGET clangDriver APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangDriver PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangDriver.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangDriver )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangDriver "${_IMPORT_PREFIX}/lib64/libclangDriver.a" )
+
+# Import target "clangSerialization" for configuration "MinSizeRel"
+set_property(TARGET clangSerialization APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangSerialization PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangSerialization.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangSerialization )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangSerialization "${_IMPORT_PREFIX}/lib64/libclangSerialization.a" )
+
+# Import target "clangRewriteFrontend" for configuration "MinSizeRel"
+set_property(TARGET clangRewriteFrontend APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangRewriteFrontend PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangRewriteFrontend.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangRewriteFrontend )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangRewriteFrontend "${_IMPORT_PREFIX}/lib64/libclangRewriteFrontend.a" )
+
+# Import target "clangFrontend" for configuration "MinSizeRel"
+set_property(TARGET clangFrontend APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangFrontend PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangFrontend.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangFrontend )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangFrontend "${_IMPORT_PREFIX}/lib64/libclangFrontend.a" )
+
+# Import target "clangFrontendTool" for configuration "MinSizeRel"
+set_property(TARGET clangFrontendTool APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangFrontendTool PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangFrontendTool.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangFrontendTool )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangFrontendTool "${_IMPORT_PREFIX}/lib64/libclangFrontendTool.a" )
+
+# Import target "clangToolingCore" for configuration "MinSizeRel"
+set_property(TARGET clangToolingCore APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangToolingCore PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangToolingCore.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangToolingCore )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangToolingCore "${_IMPORT_PREFIX}/lib64/libclangToolingCore.a" )
+
+# Import target "clangToolingRefactor" for configuration "MinSizeRel"
+set_property(TARGET clangToolingRefactor APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangToolingRefactor PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangToolingRefactor.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangToolingRefactor )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangToolingRefactor "${_IMPORT_PREFIX}/lib64/libclangToolingRefactor.a" )
+
+# Import target "clangToolingASTDiff" for configuration "MinSizeRel"
+set_property(TARGET clangToolingASTDiff APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangToolingASTDiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangToolingASTDiff.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangToolingASTDiff )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangToolingASTDiff "${_IMPORT_PREFIX}/lib64/libclangToolingASTDiff.a" )
+
+# Import target "clangTooling" for configuration "MinSizeRel"
+set_property(TARGET clangTooling APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangTooling PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangTooling.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangTooling )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangTooling "${_IMPORT_PREFIX}/lib64/libclangTooling.a" )
+
+# Import target "clangIndex" for configuration "MinSizeRel"
+set_property(TARGET clangIndex APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangIndex PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangIndex.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangIndex )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangIndex "${_IMPORT_PREFIX}/lib64/libclangIndex.a" )
+
+# Import target "clangStaticAnalyzerCore" for configuration "MinSizeRel"
+set_property(TARGET clangStaticAnalyzerCore APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangStaticAnalyzerCore PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangStaticAnalyzerCore.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangStaticAnalyzerCore )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangStaticAnalyzerCore "${_IMPORT_PREFIX}/lib64/libclangStaticAnalyzerCore.a" )
+
+# Import target "clangStaticAnalyzerCheckers" for configuration "MinSizeRel"
+set_property(TARGET clangStaticAnalyzerCheckers APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangStaticAnalyzerCheckers PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangStaticAnalyzerCheckers.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangStaticAnalyzerCheckers )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangStaticAnalyzerCheckers "${_IMPORT_PREFIX}/lib64/libclangStaticAnalyzerCheckers.a" )
+
+# Import target "clangStaticAnalyzerFrontend" for configuration "MinSizeRel"
+set_property(TARGET clangStaticAnalyzerFrontend APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangStaticAnalyzerFrontend PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangStaticAnalyzerFrontend.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangStaticAnalyzerFrontend )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangStaticAnalyzerFrontend "${_IMPORT_PREFIX}/lib64/libclangStaticAnalyzerFrontend.a" )
+
+# Import target "clangFormat" for configuration "MinSizeRel"
+set_property(TARGET clangFormat APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangFormat PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangFormat.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangFormat )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangFormat "${_IMPORT_PREFIX}/lib64/libclangFormat.a" )
+
+# Import target "clang" for configuration "MinSizeRel"
+set_property(TARGET clang APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clang PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/clang-7"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clang )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clang "${_IMPORT_PREFIX}/bin/clang-7" )
+
+# Import target "clang-format" for configuration "MinSizeRel"
+set_property(TARGET clang-format APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clang-format PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/clang-format"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clang-format )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clang-format "${_IMPORT_PREFIX}/bin/clang-format" )
+
+# Import target "clangHandleCXX" for configuration "MinSizeRel"
+set_property(TARGET clangHandleCXX APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clangHandleCXX PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_MINSIZEREL "CXX"
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclangHandleCXX.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clangHandleCXX )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clangHandleCXX "${_IMPORT_PREFIX}/lib64/libclangHandleCXX.a" )
+
+# Import target "clang-import-test" for configuration "MinSizeRel"
+set_property(TARGET clang-import-test APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clang-import-test PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/clang-import-test"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clang-import-test )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clang-import-test "${_IMPORT_PREFIX}/bin/clang-import-test" )
+
+# Import target "clang-rename" for configuration "MinSizeRel"
+set_property(TARGET clang-rename APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clang-rename PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/clang-rename"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clang-rename )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clang-rename "${_IMPORT_PREFIX}/bin/clang-rename" )
+
+# Import target "clang-refactor" for configuration "MinSizeRel"
+set_property(TARGET clang-refactor APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(clang-refactor PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/bin/clang-refactor"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS clang-refactor )
+list(APPEND _IMPORT_CHECK_FILES_FOR_clang-refactor "${_IMPORT_PREFIX}/bin/clang-refactor" )
+
+# Import target "libclang" for configuration "MinSizeRel"
+set_property(TARGET libclang APPEND PROPERTY IMPORTED_CONFIGURATIONS MINSIZEREL)
+set_target_properties(libclang PROPERTIES
+  IMPORTED_LOCATION_MINSIZEREL "${_IMPORT_PREFIX}/lib64/libclang.so.7"
+  IMPORTED_SONAME_MINSIZEREL "libclang.so.7"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libclang )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libclang "${_IMPORT_PREFIX}/lib64/libclang.so.7" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
